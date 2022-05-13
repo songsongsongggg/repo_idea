ac4d42e3d79584a213a67e0d1ee89deea1d00a70
wuzhisong <89806834+songsongsongggg@users.noreply.github.com>
2022-05-13 13:05:20 +0800

    Update UserMapper.java

--- song_edu_home_prarent/ssm-dao/src/main/java/com/song/dao/UserMapper.java
+++ song_edu_home_prarent/ssm-dao/src/main/java/com/song/dao/UserMapper.java
@@ -63,6 +63,13 @@ public interface UserMapper {
     
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
     
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
     
     
     
@@ -72,9 +79,7 @@ public interface UserMapper {
     
     
     
-    
-    
-    
+   
     
 
 }
